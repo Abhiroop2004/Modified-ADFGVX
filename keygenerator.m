032ec6ba982c73a4da64b1dcdaee237bf1ba800c
Abhiroop Sarkar <73867798+Abhiroop2004@users.noreply.github.com>
2023-10-23 10:48:50 +0530

    Update keygenerator.m

--- keygenerator.m
+++ keygenerator.m
@@ -1,9 +1,9 @@
-input("Enter length of the message you want to encrypt:");
-disp(keygenerator(l));
-function [key] = keygenerator(l)
+l=input("Enter length of the message you want to encrypt:");
+disp(length(keygen(l)));
+function [key] = keygen(l)
 %   This function generates a random key.
 %   The key is of length suitable for the length of the message being encrypted in ACDFGVX.
 %   This key should be kept secret and only be known to the attacker
-keyl=floor(sqrt(l*2));
+keyl=ceil(sqrt(l));
 key=randsample(65:90,keyl);
 end
