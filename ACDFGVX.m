f224cc821305be9fc5919ecf6612bbcf7cc786f6
Abhiroop Sarkar <73867798+Abhiroop2004@users.noreply.github.com>
2023-11-05 19:45:57 +0530

    Update ACDFGVX.m

--- ACDFGVX.m
+++ ACDFGVX.m
@@ -1,22 +1,15 @@
-%   This is an encryption system using ACDFGVX cipher (A modified version of ADFGVX, which is an upgraded version of ADFGX)
 %   The ADFGVX Cipher was used by the German Army during World War I as a field cipher. 
 %   It was an extension of the earlier ADFGX Cipher which worked in a very similar way. 
 %   It was invented by Colonel Fritz Nebel, and it combines an adapted Polybius Square with Columnar Transposition.
 %   It uses a 7x7 polybius square instead of the 6x6 in the original (or 5x5 in case of ADFGX).
 %   Source: Wikipedia, CRYPTO CORNER.
-
+function [final] = ACDFGVX(c)
 m='ACDFGVX';
 mapping=table2array(combinations(m,m));
 file=fopen("polybius.txt","r");
 poly=fscanf(file,'%c',51);
 poly=strrep(poly,'"','');
 acdfgvx=strcat(mapping,poly');
-
-disp("Welcome to ACDFGVX encrypting system:");
-disp("You have the following choices:")
-disp("1. To encrypt plain text(message)");
-disp("2. To decrypt cipher text(encrypted data)");
-c=input("Enter your choice:");
 switch c
     case 1
         %This is the code for encrypting data
@@ -37,15 +30,29 @@ switch c
         padl=ceil((l*2)/keyl)*keyl;
         message_s=pad(message_s,padl);
         disp(message_s);
-        cipher_i=reshape(message_s,[keyl,(padl/keyl)])';
-        cipher_j=selection_sort(key,cipher_i);
-        disp(cipher_j);
+        row_n=padl/keyl;
+        % Columnar Transposition
+        cipher_i=reshape(message_s,[keyl,row_n])';
+        cipher_j=selection_sort(key,cipher_i,keyl);
+        final=reshape(cipher_j,[1,padl]);
+        
+        % Row Transposition
+        %t=cipher_j(i,:);
+        %cipher_j()   
+
     case 2
-        print();
+        c=input("Enter text to decrypt:",'s');
+        key=input("Enter your secret key unique to the sender:",'s');
+        l=length(c);
+        keyl=length(key);
+        padl=ceil((l)/keyl)*keyl;
+        c=reshape(c,[padl/keyl, keyl]);
+        pt=selection_sort(key,c,keyl);
+        final=reshape(pt,[1,padl]);
         % This portion hasn't been developed yet
 end
 
-function cipher_j = selection_sort(array,array1)
+function cipher_j = selection_sort(array,array1,keyl)
   len = length(array);
   for i=1:1:len
     min=i;
@@ -54,7 +61,6 @@ function cipher_j = selection_sort(array,array1)
             min=j;
         end       
     end
-
     temp1=array1(:,i);
     array1(:,i)=array1(:,min);
     array1(:,min)=temp1;
@@ -63,4 +69,5 @@ function cipher_j = selection_sort(array,array1)
     array(:,min)=temp;
   end
   cipher_j=char(array1);
+end
 end
